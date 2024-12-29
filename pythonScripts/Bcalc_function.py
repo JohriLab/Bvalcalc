Fix E_bar calculation

pythonScripts/Bcalc_function.py
@@ -3,6 +3,10 @@ import numpy as np
 from constants import g, tract_len, r, u, l, U, Nanc, gamma_cutoff, h, t0, t1, t1half, t2, t3, t4, f0, f1, f2, f3
 
 def calculate_B(distance_to_element, length_of_element):
+    """
+    Calculate the B value for a single functional element at the focal site,
+    summing over the DFE while consolidating the intermediate calculations.
+    """
     # Calculate "a" and "b"
     C = (1.0 - np.exp(-2.0 * r * distance_to_element)) / 2.0
     if g == 0:
@@ -21,22 +25,22 @@ def calculate_B(distance_to_element, length_of_element):
             g * tract_len + r * length_of_element + C
         )
 
-    # Helper to calculate exponent using "a" and "b"
+    # Helper to calculate the exponent using "a" and "b"
     def calculate_exponent(t_start, t_end):
         E1 = ((U * a) / ((1 - a) * (a - b) * (t_end - t_start))) * np.log((a + (t_end * (1 - a))) / (a + (t_start * (1 - a))))
         E2 = -1.0 * ((U * b) / ((1 - b) * (a - b) * (t_end - t_start))) * np.log((b + ((1 - b) * t_end)) / (b + ((1 - b) * t_start)))
-        return E1 + E2
+        return E1 + E2 # = E
 
     # Calculate exponents for different time intervals
     E_f1 = calculate_exponent(t1half, t2)
     E_f2 = calculate_exponent(t2, t3)
     E_f3 = calculate_exponent(t3, t4)
 
-    # Calculate E_bar
+    # Calculate E_bar (sum over DFE)
     E_bar = (
         f0 * 0.0
-        + f1 * ((t1half - t1half) / (t2 - t1half)) * 0.0 ## @Parul is this supposed to be 0?
-        + f1 * ((t2 - t1half) / (t2 - t1half)) * E_f1
+        + f1 * ((t1half - t1) / (t2 - t1)) * 0.0
+        + f1 * ((t2 - t1half) / (t2 - t1)) * E_f1
         + f2 * E_f2
         + f3 * E_f3
     )