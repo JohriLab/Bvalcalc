Fix U calculation with l, changed to length_of_element

pythonScripts/Bcalc_function.py
@@ -1,6 +1,6 @@
 import math
 import numpy as np
-from constants import g, tract_len, r, u, l, U, Nanc, gamma_cutoff, h, t0, t1, t1half, t2, t3, t4, f0, f1, f2, f3
+from constants import g, tract_len, r, u, Nanc, gamma_cutoff, h, t0, t1, t1half, t2, t3, t4, f0, f1, f2, f3
 
 def calculate_B(distance_to_element, length_of_element):
     """
@@ -9,6 +9,7 @@ def calculate_B(distance_to_element, length_of_element):
     """
     # Calculate "a" and "b"
     C = (1.0 - np.exp(-2.0 * r * distance_to_element)) / 2.0
+    U = length_of_element * u
     if g == 0:
         a = C
         b = C + r * length_of_element

pythonScripts/Bcalc_multiElements.py
@@ -102,8 +102,10 @@ sites['B'][unique_indices] = aggregated_B
 for start, end in zip(blockstart, blockend):
     sites['B'][start - sites['pos'][0]:end - sites['pos'][0]] = np.nan 
 
-print(np.sort(sites[~np.isnan(sites['B'])], order=['B'])) # Removes gene sites and prints
+nogene_sites = np.sort(sites[~np.isnan(sites['B'])], order=['B']) # Removes gene sites
+# print(nogene_sites)
 # print(sites)
+print(calculate_B(2,20000))
 sys.exit()
 
 #calculate an average B value over the window with coordinates win_start - win_end