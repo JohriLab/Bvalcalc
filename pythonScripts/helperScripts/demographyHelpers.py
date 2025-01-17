5a1b8baf72c76d1e70b0a28ab1231d181c6da03c
Jacob Marsh <jake.marsh@live.com.au>
2025-01-17 13:45:45 -0500

    Minor refactoring, add b_cur calculation

---
+++ pythonScripts/helperScripts/demographyHelpers.py
@@ -0,0 +1,10 @@
+import numpy as np
+from constants import Nanc, Ncur, time_of_change
+
+# #Gets the value of B in the current population as a function of B calcualted in the ancestral population (assumed to be in equilibrium).
+# #As in, this is where we account for a simple single-size change in N
+
+def get_Bcur(Banc):
+    R = float(Nanc)/float(Ncur)
+    Bcur = (Banc*(1.0 + (R-1.0)*np.exp((-1.0*time_of_change)/Banc))) / (1 + (R-1.0)*np.exp(-1.0*time_of_change)) ##Currently time_of_change is hardcoded, need to change to variable
+    return (Bcur)
---
+++ pythonScripts/helperScripts/runBcalc.py
@@ -0,0 +1,44 @@
+from helperScripts.process_single_chunk import process_single_chunk
+from helperScripts.bedgffHandler import bedgffHandler
+from helperScripts.calculateLPerChunk import calculateLPerChunk
+from helperScripts.demographyHelpers import get_Bcur
+from constants import g, tract_len, r, u, Ncur, Nanc, gamma_cutoff, h, t0, t1, t1half, t2, t3, t4, f0, f1, f2, f3, time_of_change
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import numpy as np
+
+#Main function
+def runBcalc(args):    
+    file_path, chr_start, chr_end, chunk_size, precise_chunks = args.file_path, args.chr_start, args.chr_end, args.chunk_size, args.precise_chunks
+    num_chunks = (chr_end - chr_start + chunk_size - 1) // chunk_size
+
+    # Read BED/GFF and return genes and relevant flanking regions for calculating B
+    blockstart, blockend =  \
+        bedgffHandler(file_path) 
+
+    # Initialize the array for B values (all initially set to 1.0)
+    b_values = np.ones(chr_end - chr_start, dtype=np.float64)
+
+    # Calculate cumulative conserved length in each chunk 
+    lperchunk = calculateLPerChunk(chunk_size, blockstart, blockend, chr_start, chr_end)
+
+    # # Iterate over chunks, calculating B for all neutral sites
+    # for chunk_num in range(num_chunks): #Iterate through each chunk (Old loop)
+    #     b_values = \
+    #     process_single_chunk(chunk_num, chunk_size, blockstart, blockend, chr_start, chr_end, num_chunks, precise_chunks, lperchunk, b_values)
+
+    with ThreadPoolExecutor() as executor:
+        results = [executor.submit(process_single_chunk, x, chunk_size, blockstart, blockend, chr_start, 
+                                   chr_end, num_chunks, precise_chunks, lperchunk, b_values)
+            for x in range(num_chunks)]
+        
+    if args.pop_change:
+        return get_Bcur(b_values)
+    else:
+        return b_values
+    
+        # # Converts B at gene sites to 'nan'
+    # for start, end in zip(blockstart, blockend):
+    #     sites['B'][start - sites['pos'][0]:end - sites['pos'][0]] = np.nan 
+
+    # nogene_sites = np.sort(sites[~np.isnan(sites['B'])], order=['B']) # Removes gene sites
+    # print(nogene_sites)
