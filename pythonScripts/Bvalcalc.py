ce798fcf9698f54bc889e226ed224050c982f035
Jacob Marsh <jake.marsh@live.com.au>
2025-02-08 19:46:46 -0500

    Add group parser for genome, region. Renamed Bcalc.py to Bvalcalc.py

--- pythonScripts/Bvalcalc.py
+++ pythonScripts/Bvalcalc.py
@@ -6,22 +6,33 @@
 #This script is to get B values across a genomic element with multiple functional elements.
 #Currently the output is in terms of an average of a sliding window
 import sys
-from helperScripts.parseArgs import parseGenomeArgs
-from helperScripts.parseArgs import parseRegionArgs
+from helperScripts.parseArgs import parseGenomeArgs, parseRegionArgs
 from helperScripts.genomeBcalc import genomeBcalc
 from helperScripts.regionBcalc import regionBcalc
 from helperScripts.plotBasic import plotBasic
 from helperScripts.calculateB import calculateB
 import time
 
+import argparse
+
 #CLI handling
 def main():
     start_time = time.time()
 
-    args = parseGenomeArgs() 
-    genomeBcalc(args)
-    # args = parseRegionArgs()
-    # regionBcalc(args)
+    parser = argparse.ArgumentParser(description="Bcalc main! :p")
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('--genome', action='store_true', help="Compute B values genome-wide")
+    group.add_argument('--region', action='store_true', help="Compute B values for a region")
+
+    known_args, remaining_args = parser.parse_known_args()
+
+    if known_args.genome:
+        args = parseGenomeArgs(remaining_args)
+        genomeBcalc(args)
+    elif known_args.region:
+        args = parseRegionArgs(remaining_args)
+        regionBcalc(args)
+
     # plotBasic(args)
     # print("4k test", calculateB(4000, 10000))
     # print("1.25k test", calculateB(1250, 10000))
--- pythonScripts/helperScripts/parseArgs.py
+++ pythonScripts/helperScripts/parseArgs.py
@@ -1,6 +1,6 @@
 import argparse
 
-def parseGenomeArgs():
+def parseGenomeArgs(argv=None):
     parser = argparse.ArgumentParser(description="Calculates B for all neutral sites across given chromosome.")
     parser.add_argument('--chr_start', type=int, required=True, help="Start of chromosome range.")
     parser.add_argument('--chr_end', type=int, required=True, help="End of chromosome range.")
@@ -9,11 +9,11 @@ def parseGenomeArgs():
     parser.add_argument('--file_path', type=str, required=True, help="Path to input BED or GFF3 file.")
     parser.add_argument('--precise_chunks', type=int, default=3, help="Number of adjacent chunks to calculate B precisely.")
     parser.add_argument('--pop_change', action='store_false', help="If set, B will reflect the current B after a step change in population size, rather than ancestral B.")
-    return parser.parse_args()
+    return parser.parse_args(argv)
 
-def parseRegionArgs():
+def parseRegionArgs(argv=None):
     parser = argparse.ArgumentParser(description="Calculates B for neutral sites flanking a single region under selection.")
     parser.add_argument('--gene_size', type=int, default=10000, help="Length of single region (e.g. gene) under selection. [5000]")
     parser.add_argument('--flank_len', type=int, default=25000, help="Length of flanking neutral region for which to calcuate recovery of B. [25000]")
     parser.add_argument('--pop_change', action='store_false', help="If set, B will reflect the current B after a step change in population size, rather than ancestral B.")
-    return parser.parse_args()
+    return parser.parse_args(argv)
