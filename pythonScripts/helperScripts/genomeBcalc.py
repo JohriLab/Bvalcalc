ba44a9a26fbae6526213b0ec1a20fb57472655b5
Jacob Marsh <jake.marsh@live.com.au>
2025-02-08 20:24:51 -0500

    Add std out print statements

--- pythonScripts/helperScripts/genomeBcalc.py
+++ pythonScripts/helperScripts/genomeBcalc.py
@@ -8,6 +8,17 @@ import numpy as np
 #Main function
 def genomeBcalc(args):    
     file_path, chr_start, chr_end, chunk_size, precise_chunks = args.file_path, args.chr_start, args.chr_end, args.chunk_size, args.precise_chunks
+
+    print(f"Calculating relative diversity (B) for all neutral sites across the genome...")
+
+    print(f"====== P A R A M E T E R S =========================")
+    print(f"BED/GFF file for regions under selection: {file_path}")
+    print(f"First position in chromosssssssssssssome: {chr_start}")
+    print(f"Last position in chromosome: {chr_end}")
+    print(f"Size of chunks to calculate B in per iteration: {chunk_size}bp")
+    print(f"Number of adjacent chunks to calculate B precisely for: {precise_chunks}bp")
+
+
     num_chunks = (chr_end - chr_start + chunk_size - 1) // chunk_size
 
     # Read BED/GFF and return genes and relevant flanking regions for calculating B
@@ -28,13 +39,15 @@ def genomeBcalc(args):
     for s, e in zip(blockstart, blockend): # Converts gene sites to NaN
         b_values[s - chr_start : e - chr_start + 1] = np.nan
 
+    print(f"====== R E S U L T S == P E R == C H U N K =========")
     with ThreadPoolExecutor() as executor:
         results = [executor.submit(process_single_chunk, x, chunk_size, blockstart, blockend, chr_start, 
                                    chr_end, num_chunks, precise_chunks, lperchunk, b_values)
             for x in range(num_chunks)]
-        
-    print("Mean B (no nan sites):", b_values[~np.isnan(b_values)].mean())
-    print("Total conserved length: ", int(sum(lperchunk)), round((sum(lperchunk)/(chr_end - chr_start))*100,2), "%" )
+    
+    print(f"====== R E S U L T S ====== S U M M A R Y ===========")
+    print(f"Mean B of neutral sites across genome: {b_values[~np.isnan(b_values)].mean()}bp")
+    print(f"Cumulative length of regions under selection: {int(sum(lperchunk))}bp ({round((sum(lperchunk)/(chr_end - chr_start))*100,2)}%)")
 
     if args.pop_change:
         return b_values#get_Bcur(b_values)
--- pythonScripts/helperScripts/regionBcalc.py
+++ pythonScripts/helperScripts/regionBcalc.py
@@ -4,12 +4,19 @@ import numpy as np
 
 def regionBcalc(args):    
     gene_size, flank_len = args.gene_size, args.flank_len
+    print(f"Calculating relative diversity (B) for a neutral region adjacent to a single selected region (e.g. gene, exon, regulatory element)...")
+
+    print(f"====== P A R A M E T E R S =========================")
+    print(f"Distribution of fitness effects (DFE): {flank_len}bp")
+    print(f"Length of region under selection: {gene_size}bp")
+    print(f"Length of flanking neutral region: {flank_len}bp")
 
     b_values = calculateB(np.arange(1, flank_len, 1, dtype = int), gene_size)
 
+    print(f"====== R E S U L T S ! =============================")
     print(f"B for adjacent site: {b_values[0]}")
     print(f"Mean B for flanking region: {b_values.mean()}")
-    print(f"All B: {b_values}")
+    print(f"B at start and end of the neutral region: {b_values}")
 
     if args.pop_change:
         return get_Bcur(b_values)
