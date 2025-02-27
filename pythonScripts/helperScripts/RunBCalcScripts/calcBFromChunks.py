Fixed off-by-one error in rec distances, and added final chunk distance check

pythonScripts/helperScripts/RunBCalcScripts/calcBFromChunks.py
@@ -24,8 +24,8 @@ def calcBFromChunks(chunk_index, chunk_size, blockstart, blockend, chr_start, ch
 
     relevant_upstream_pseudoblockends = chunk_pseudoblockend[upstream_pseudochunk_mask]
     relevant_downstream_pseudoblockstarts = chunk_pseudoblockstart[downstream_pseudochunk_mask]
-    relevant_upstream_psdc_distances = chunk_mids[chunk_index] - relevant_upstream_pseudoblockends
-    relevant_downstream_psdc_distances = relevant_downstream_pseudoblockstarts - chunk_mids[chunk_index]
+    relevant_upstream_psdc_distances = chunk_mids[chunk_index] - relevant_upstream_pseudoblockends - 1
+    relevant_downstream_psdc_distances = relevant_downstream_pseudoblockstarts - chunk_mids[chunk_index] - 1
 
     if rec_rate_per_chunk is not None: # IF REC_RATE MAP IS AVAILABLE 
         # Get the indices for upstream and downstream pseudochunks
@@ -37,7 +37,7 @@ def calcBFromChunks(chunk_index, chunk_size, blockstart, blockend, chr_start, ch
             upstream_indices, downstream_indices, rec_rate_per_chunk, 
             relevant_upstream_psdc_lengths, relevant_downstream_psdc_lengths, 
             chunk_index, chunk_size, relevant_upstream_pseudoblockends, relevant_downstream_pseudoblockstarts, 
-            chunk_starts, chunk_ends, chunk_rec_distances, relevant_upstream_psdc_distances
+            chunk_starts, chunk_ends, chunk_rec_distances, num_chunks
             ) # Get local r * lengths for length of, and distances to pseudoblocks for each chunk
 
         relevant_upstream_psdc_B = np.prod(calculateB_recmap(relevant_upstream_psdc_distances, relevant_upstream_psdc_lengths, upstream_rec_distances, upstream_rec_lengths))

pythonScripts/helperScripts/RunBCalcScripts/process_single_chunk.py
@@ -27,7 +27,7 @@ def process_single_chunk(chunk_num, chunk_size, blockstart, blockend,
     
     if not np.any(not_nan_mask):
         # Everything is already NaN in this chunk, no need to do anything
-        print(f"No neutral sites in chunk {chunk_num}: {chunk_start}-{chunk_end}")
+        # print(f"No neutral sites in chunk {chunk_num}: {chunk_start}-{chunk_end}")
         return b_values
     
     # Filter positions to skip pre-labeled NaNs
@@ -106,7 +106,7 @@ def process_single_chunk(chunk_num, chunk_size, blockstart, blockend,
     # If there are no elements left, default flank_B to 1
     if flat_distances.size == 0 or flat_lengths.size == 0:
         flank_B = 1
-        print(f"No nearby sites under selection in flanking region for chunk:", chunk_num)
+        # print(f"No nearby sites under selection in flanking region for chunk:", chunk_num)
 
     true_indices = np.where(flanking_mask)
     unique_indices, inverse_indices = np.unique(true_indices[1], return_inverse=True)
@@ -125,16 +125,16 @@ def process_single_chunk(chunk_num, chunk_size, blockstart, blockend,
     chunk_slice[not_nan_mask] = chunk_slice_clean
     mean_chunk_b = np.nanmean(chunk_slice) # Mean B for chunk
 
-    print(f"Processing chunk: {pos_chunk.min()} - {pos_chunk.max()}")
-            # Check if recombination rate data is provided and print it for the current chunk.
-    if rec_rate_per_chunk is not None:
-        rec_rate = rec_rate_per_chunk[chunk_num]
-        print(f"Chunk {chunk_num}: recombination rate = {rec_rate}")
-    print(f"B from distant chunks: {B_from_distant_chunks}")
-    print(f"Number of relevant genes: {len(precise_blockstart)}")
-    # print(f"Relevant blocks: {precise_blockstart}, {precise_blockend}")
-    print(f"Number of neutral sites in chunk [{chunk_start}-{chunk_end}): {np.isnan(chunk_slice).sum()}")
-    print(f"Aggregated B values for chunk: {aggregated_B}")
-    print(f"Mean B value for chunk {chunk_num}: [{chunk_start}-{chunk_end}]: {mean_chunk_b}")
+    # print(f"Processing chunk: {pos_chunk.min()} - {pos_chunk.max()}")
+    #         # Check if recombination rate data is provided and print it for the current chunk.
+    # if rec_rate_per_chunk is not None:
+    #     rec_rate = rec_rate_per_chunk[chunk_num]
+    #     print(f"Chunk {chunk_num}: recombination rate = {rec_rate}")
+    # print(f"B from distant chunks: {B_from_distant_chunks}")
+    # print(f"Number of relevant genes: {len(precise_blockstart)}")
+    # # print(f"Relevant blocks: {precise_blockstart}, {precise_blockend}")
+    # print(f"Number of neutral sites in chunk [{chunk_start}-{chunk_end}): {np.isnan(chunk_slice).sum()}")
+    # print(f"Aggregated B values for chunk: {aggregated_B}")
+    # print(f"Mean B value for chunk {chunk_num}: [{chunk_start}-{chunk_end}]: {mean_chunk_b}")
 
     return b_values