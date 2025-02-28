46f4e49d60596844a3ca33605027b688e586240a
Jacob Marsh <jake.marsh@live.com.au>
2025-02-27 22:26:05 -0500

    Formatting code

--- pythonScripts/helperScripts/RunBCalcScripts/process_single_chunk.py
+++ pythonScripts/helperScripts/RunBCalcScripts/process_single_chunk.py
@@ -1,41 +1,28 @@
-import sys
-import numpy as np
 from helperScripts.calculateB import calculateB_linear, calculateB_recmap
 from helperScripts.RunBCalcScripts.calcBFromChunks import calcBFromChunks
 from helperScripts.RunBCalcScripts.recmapHandler import calcRLengths
 from helperScripts.RunBCalcScripts.recmapHandler import calcRDistances
+import numpy as np
 
 def process_single_chunk(chunk_num, chunk_size, blockstart, blockend,
                          chr_start, chr_end, num_chunks, precise_chunks,
                          lperchunk, b_values, rec_rate_per_chunk=None):
-    
-    # Compute chunk boundaries
+
     chunk_start = chr_start + chunk_num * chunk_size
     chunk_end   = min(chunk_start + chunk_size, chr_end)
-    
-    # Array of positions in this chunk
-    pos_chunk = np.arange(chunk_start, chunk_end)
 
-    # Slice b_values for this chunk
-    start_idx = chunk_start - chr_start
-    end_idx   = chunk_end - chr_start
-    chunk_slice = b_values[start_idx:end_idx]
-    # chunk_slice.shape == (len(pos_chunk),)
-
-    # Make a mask for positions that are NOT NaN
-    not_nan_mask = ~np.isnan(chunk_slice)
+    chunk_slice = b_values[chunk_start - chr_start:chunk_end - chr_start] # Get b_values for this chunk
+    not_nan_mask = ~np.isnan(chunk_slice) # Make a mask for positions that are NOT NaN
     
     if not np.any(not_nan_mask):
-        # Everything is already NaN in this chunk, no need to do anything
-        # print(f"No neutral sites in chunk {chunk_num}: {chunk_start}-{chunk_end}")
+        print(f"No neutral sites in chunk {chunk_num}: {chunk_start}-{chunk_end}")
         return b_values
     
-    # Filter positions to skip pre-labeled NaNs
+    pos_chunk = np.arange(chunk_start, chunk_end) # Array of positions in this chunk
     pos_chunk_clean   = pos_chunk[not_nan_mask] # Positions of neutral sites
     chunk_slice_clean = chunk_slice[not_nan_mask]
 
-    # == 1) If you rely on B_from_distant_chunks, compute that as usual ==
-    B_from_distant_chunks = calcBFromChunks(
+    B_from_distant_chunks = calcBFromChunks( # Compute B from distant chunks in non-precise region
         chunk_num, chunk_size,
         blockstart, blockend,
         chr_start, chr_end,
@@ -44,10 +31,9 @@ def process_single_chunk(chunk_num, chunk_size, blockstart, blockend,
         rec_rate_per_chunk
     )
 
-    # == 2) Identify blocks in the "precise region" (unchanged) ==
+    # Identify blocks in the "precise region"
     precise_region_start = np.maximum(chr_start, chr_start + (chunk_num - precise_chunks) * chunk_size)
     precise_region_end   = np.minimum(chr_end, chr_start + (chunk_num + 1 + precise_chunks) * chunk_size - 1)
-
     precise_blockregion_mask = (
         (precise_region_end   >= blockstart) &
         (precise_region_start <= blockend)
@@ -56,26 +42,19 @@ def process_single_chunk(chunk_num, chunk_size, blockstart, blockend,
                                  a_min=precise_region_start, a_max=precise_region_end)
     precise_blockend   = np.clip(blockend[precise_blockregion_mask],
                                  a_min=precise_region_start, a_max=precise_region_end)
-    # print(precise_blockend)
 
+    physical_distances_upstream   = pos_chunk_clean[None, :] - precise_blockend[:, None] # All distances to blockends (upstream and downstream)
+    physical_distances_downstream = precise_blockstart[:, None] - pos_chunk_clean[None, :] # All distances to blockstarts (upstream and downstream)
 
-
-    # # == 3) Do distance calculations ONLY for non-NaN sites ==
     downstream_mask = (pos_chunk_clean < precise_blockstart[:, None])
     upstream_mask   = (pos_chunk_clean > precise_blockend[:, None])
     flanking_mask   = downstream_mask | upstream_mask
-
-    physical_distances_upstream   = pos_chunk_clean[None, :] - precise_blockend[:, None]
-    # if chunk_num == 12:
-    #     print("HERE1st", np.sum(physical_distances_upstream), chunk_num)
-    physical_distances_downstream = precise_blockstart[:, None] - pos_chunk_clean[None, :]
-    physical_distances = np.where(
+    physical_distances = np.where( # Filter so only distances to blockends upstream and blockstarts downstream kept
         flanking_mask,
         np.where(upstream_mask, physical_distances_upstream, physical_distances_downstream),
         np.nan
     )
-
-    flat_distances = physical_distances[flanking_mask]
+    flat_distances = physical_distances[flanking_mask] # Flatten array
 
     physical_lengths = precise_blockend - precise_blockstart
     flat_lengths   = np.repeat(physical_lengths, flanking_mask.sum(axis=1))
@@ -103,38 +82,26 @@ def process_single_chunk(chunk_num, chunk_size, blockstart, blockend,
     else:
         flank_B = calculateB_linear(flat_distances, flat_lengths)
 
-    # If there are no elements left, default flank_B to 1
     if flat_distances.size == 0 or flat_lengths.size == 0:
         flank_B = 1
-        # print(f"No nearby sites under selection in flanking region for chunk:", chunk_num)
+        print(f"No nearby sites under selection in flanking region for chunk:", chunk_num)
 
-    true_indices = np.where(flanking_mask)
-    unique_indices, inverse_indices = np.unique(true_indices[1], return_inverse=True)
-
+    unique_indices, inverse_indices = np.unique(np.where(flanking_mask)[1], return_inverse=True)
     aggregated_B = np.ones_like(unique_indices, dtype=np.float64)
-    np.multiply.at(aggregated_B, inverse_indices, flank_B)
+    np.multiply.at(aggregated_B, inverse_indices, flank_B) # Multiplicative sum of B calculated at a given site from multiple elements
 
-    # == 4) Write results only to the non-NaN slice ==
-    global_indices_clean = pos_chunk_clean[unique_indices] - chr_start
-    # Now map them into chunk_slice_clean indices:
-    chunk_slice_clean_indices = unique_indices  # because pos_chunk_clean -> chunk_slice_clean is 1:1
-    chunk_slice_clean[chunk_slice_clean_indices] *= (aggregated_B * B_from_distant_chunks)
-
-    # Put the updated (non-NaN) slice back into the original b_values
-    # The NaNs remain untouched
-    chunk_slice[not_nan_mask] = chunk_slice_clean
+    chunk_slice_clean[unique_indices] *= (aggregated_B * B_from_distant_chunks) # Update chunk slice and combine flank_B with B from distant chunks
+    chunk_slice[not_nan_mask] = chunk_slice_clean # Put the updated (non-NaN) slice back into the original b_values
     mean_chunk_b = np.nanmean(chunk_slice) # Mean B for chunk
 
-    # print(f"Processing chunk: {pos_chunk.min()} - {pos_chunk.max()}")
-    #         # Check if recombination rate data is provided and print it for the current chunk.
-    # if rec_rate_per_chunk is not None:
-    #     rec_rate = rec_rate_per_chunk[chunk_num]
-    #     print(f"Chunk {chunk_num}: recombination rate = {rec_rate}")
-    # print(f"B from distant chunks: {B_from_distant_chunks}")
-    # print(f"Number of relevant genes: {len(precise_blockstart)}")
-    # # print(f"Relevant blocks: {precise_blockstart}, {precise_blockend}")
-    # print(f"Number of neutral sites in chunk [{chunk_start}-{chunk_end}): {np.isnan(chunk_slice).sum()}")
-    # print(f"Aggregated B values for chunk: {aggregated_B}")
-    # print(f"Mean B value for chunk {chunk_num}: [{chunk_start}-{chunk_end}]: {mean_chunk_b}")
+    print(f"Processing chunk: {pos_chunk.min()} - {pos_chunk.max()}")
+    if rec_rate_per_chunk is not None:
+        rec_rate = rec_rate_per_chunk[chunk_num]
+        print(f"Chunk {chunk_num}: recombination rate = {rec_rate}")
+    print(f"B from distant chunks: {B_from_distant_chunks}")
+    print(f"Number of relevant genes: {len(precise_blockstart)}")
+    print(f"Number of neutral sites in chunk [{chunk_start}-{chunk_end}): {np.isnan(chunk_slice).sum()}")
+    print(f"Aggregated B values for chunk: {aggregated_B}")
+    print(f"Mean B value for chunk {chunk_num}: [{chunk_start}-{chunk_end}]: {mean_chunk_b}")
 
     return b_values
--- pythonScripts/helperScripts/genomeBcalc.py
+++ pythonScripts/helperScripts/genomeBcalc.py
@@ -2,16 +2,14 @@ from helperScripts.RunBCalcScripts.process_single_chunk import process_single_ch
 from helperScripts.RunBCalcScripts.bedgffHandler import bedgffHandler
 from helperScripts.RunBCalcScripts.calculateLPerChunk import calculateLPerChunk
 from helperScripts.RunBCalcScripts.demographyHelpers import get_Bcur
-from helperScripts.RunBCalcScripts.recmapHandler import recmapHandler, calcRLengths
-from concurrent.futures import ThreadPoolExecutor, as_completed
+from helperScripts.RunBCalcScripts.recmapHandler import recmapHandler
+from concurrent.futures import ThreadPoolExecutor
 import numpy as np
 
-#Main function
 def genomeBcalc(args):    
     file_path, chr_start, chr_end, chunk_size, precise_chunks = args.file_path, args.chr_start, args.chr_end, args.chunk_size, args.precise_chunks
 
     print(f"Calculating relative diversity (B) for all neutral sites across the genome...")
-
     print(f"====== P A R A M E T E R S =========================")
     print(f"BED/GFF file for regions under selection: {file_path}")
     print(f"First position in chromosome: {chr_start}")
@@ -19,54 +17,32 @@ def genomeBcalc(args):
     print(f"Size of chunks to calculate B in per iteration: {chunk_size}bp")
     print(f"Number of adjacent chunks to calculate B precisely for: {precise_chunks}")
 
+    blockstart, blockend = bedgffHandler(file_path) # Read BED/GFF, return start and end of conserved elements
+    b_values = np.ones(chr_end - chr_start, dtype=np.float64) # Initialize array of B values
+    for s, e in zip(blockstart, blockend): # Converts gene sites to NaN
+        b_values[s - chr_start : e - chr_start + 1] = np.nan
 
-    num_chunks = (chr_end - chr_start + chunk_size - 1) // chunk_size
+    lperchunk = calculateLPerChunk(chunk_size, blockstart, blockend, chr_start, chr_end) # Cumulative conserved length in each chunk
 
-    # Read BED/GFF and return genes and relevant flanking regions for calculating B
-    blockstart, blockend =  \
-        bedgffHandler(file_path) 
-
-        # Process recombination map if provided
-    if args.rec_map:
+    if args.rec_map: # Process recombination map if provided
         print(f"Using recombination map from {args.rec_map}")
         rec_rate_per_chunk = recmapHandler(args.rec_map, chr_start, chr_end, chunk_size)
     else:
         rec_rate_per_chunk = None
 
-    # blockRLengths = calcRLengths(blockstart, blockend, rec_rate_per_chunk, chr_end, chr_start, chunk_size)
-
-    # Initialize the array for B values (all initially set to 1.0)
-    b_values = np.ones(chr_end - chr_start, dtype=np.float64)
-
-    # Calculate cumulative conserved length in each chunk 
-    lperchunk = calculateLPerChunk(chunk_size, blockstart, blockend, chr_start, chr_end)
-
-    # # Iterate over chunks, calculating B for all neutral sites
-    # for chunk_num in range(num_chunks): #Iterate through each chunk (Old loop)
-    #     b_values = \
-    #     process_single_chunk(chunk_num, chunk_size, blockstart, blockend, chr_start, chr_end, num_chunks, precise_chunks, lperchunk, b_values)
-    for s, e in zip(blockstart, blockend): # Converts gene sites to NaN
-        b_values[s - chr_start : e - chr_start + 1] = np.nan
-
     print(f"====== R E S U L T S == P E R == C H U N K =========")
+
+    num_chunks = (chr_end - chr_start + chunk_size - 1) // chunk_size
     with ThreadPoolExecutor() as executor:
         results = [executor.submit(process_single_chunk, x, chunk_size, blockstart, blockend, chr_start, 
                                    chr_end, num_chunks, precise_chunks, lperchunk, b_values, rec_rate_per_chunk)
             for x in range(num_chunks)]
     
-    print(f"====== R E S U L T S ====== S U M M A R Y ===========")
+    print(f"====== R E S U L T S ====== S U M M A R Y ==========")
     print(f"Mean B of neutral sites across genome: {b_values[~np.isnan(b_values)].mean()}")
     print(f"Cumulative length of regions under selection: {int(sum(lperchunk))}bp ({round((sum(lperchunk)/(chr_end - chr_start))*100,2)}%)")
 
-
     if args.pop_change:
-        return b_values#get_Bcur(b_values)
+        return get_Bcur(b_values)
     else:
         return b_values
-    
-        # # Converts B at gene sites to 'nan'
-    # for start, end in zip(blockstart, blockend):
-    #     sites['B'][start - sites['pos'][0]:end - sites['pos'][0]] = np.nan 
-
-    # nogene_sites = np.sort(sites[~np.isnan(sites['B'])], order=['B']) # Removes gene sites
-    # print(nogene_sites)
