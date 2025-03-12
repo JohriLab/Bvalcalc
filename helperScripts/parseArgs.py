b16e8cf30738d4d1f8e8ad57847b566dfdca3cf6
Jacob Marsh <jake.marsh@live.com.au>
2025-03-11 20:35:36 -0400

    restructure file system, remove pythonScripts, move up one directory

--- helperScripts/parseArgs.py
+++ helperScripts/parseArgs.py
@@ -42,7 +42,9 @@ def parseRegionArgs(argv=None):
     parser.add_argument('--gene_size', type=int, default=10000, help="Length of single region (e.g. gene) under selection. [5000]")
     parser.add_argument('--flank_len', type=int, default=40000, help="Length of flanking neutral region for which to calcuate recovery of B. [25000]")
     parser.add_argument('--pop_change', action='store_true', help="If set, B will reflect the current B after a step change in population size, rather than ancestral B.")
-    parser.add_argument('--plot_basic', action='store_true', help="Generate a basic plot using `Bvalcalc.py --region` output")
+    parser.add_argument('--plot_output', nargs='?', default=None, 
+                        help="Generate a basic plot using `Bvalcalc.py --genome` output"
+                            "Provide path to plot output.")
     parser.add_argument('--out', nargs='?', default=None,
                         help="Optional path to output CSV file. If --out is specified but no file name is given, "
                              "'b_values.csv' will be used in the current directory. If --out is not specified, "
