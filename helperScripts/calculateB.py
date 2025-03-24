92fe29a336b70a0001d8fb10a6517d103597cb83
Jacob Marsh <jake.marsh@live.com.au>
2025-03-24 12:52:39 -0400

    Recmap included in B calcs for genic sites

--- helperScripts/calculateB.py
+++ helperScripts/calculateB.py
@@ -71,53 +71,56 @@ def calculateB_recmap(distance_to_element, length_of_element,
     Calculate the B value WITH REC MAP for a single functional element at the focal site,
     summing over the DFE while consolidating the intermediate calculations.
     """    
-    # rec_distances is the length of the element * rec rate in each spanned region. 
-    if rec_distances is not None:
-        rec_adjusted_length_of_element = rec_lengths 
-        rec_adjusted_distance_to_element = rec_distances
-    else:
-        rec_adjusted_length_of_element = length_of_element
-        rec_adjusted_distance_to_element = distance_to_element
-    
-    if gc_distances is not None:
-        gc_adjusted_length_of_element = gc_lengths 
-        gc_adjusted_distance_to_element = gc_distances
-        g_k = (gc_lengths / length_of_element) * g * k
-    else:
-        gc_adjusted_length_of_element = length_of_element
-        gc_adjusted_distance_to_element = distance_to_element
-        g_k = g * k
+    with np.errstate(divide='ignore', invalid='ignore'):
+        # rec_distances is the length of the element * rec rate in each spanned region. 
+        if rec_distances is not None:
+            rec_adjusted_length_of_element = rec_lengths 
+            rec_adjusted_distance_to_element = rec_distances
+        else:
+            rec_adjusted_length_of_element = length_of_element
+            rec_adjusted_distance_to_element = distance_to_element
         
-    C = (1.0 - np.exp(-2.0 * r * rec_adjusted_distance_to_element)) / 2.0 # cM
-    U = length_of_element * u
-    if g == 0:
-        a = C
-        b = C + r * rec_adjusted_length_of_element # cM
-    elif g > 0:
-        threshold = gc_adjusted_distance_to_element + gc_adjusted_length_of_element < 0.5 * k # Arbitrary threshold
-        a = np.where(
-            threshold, 
-            C + (g * gc_adjusted_distance_to_element), #If TRUE
-            C + g_k #If FALSE
-        )
-        b = np.where(
-            threshold,
-            C + r * rec_adjusted_length_of_element + (g * (gc_adjusted_distance_to_element + gc_adjusted_length_of_element)), #If TRUE
-            C + g_k + r * rec_adjusted_length_of_element #If FALSE
-        )
+        if gc_distances is not None:
+            gc_adjusted_length_of_element = gc_lengths 
+            gc_adjusted_distance_to_element = gc_distances
+            g_k = (gc_lengths / length_of_element) * g * k
+        else:
+            gc_adjusted_length_of_element = length_of_element
+            gc_adjusted_distance_to_element = distance_to_element
+            g_k = g * k
+            
+        C = (1.0 - np.exp(-2.0 * r * rec_adjusted_distance_to_element)) / 2.0 # cM
+        U = length_of_element * u
+        if g == 0:
+            a = C
+            b = C + r * rec_adjusted_length_of_element # cM
+        elif g > 0:
+            threshold = gc_adjusted_distance_to_element + gc_adjusted_length_of_element < 0.5 * k # Arbitrary threshold
+            a = np.where(
+                threshold, 
+                C + (g * gc_adjusted_distance_to_element), #If TRUE
+                C + g_k #If FALSE
+            )
+            b = np.where(
+                threshold,
+                C + r * rec_adjusted_length_of_element + (g * (gc_adjusted_distance_to_element + gc_adjusted_length_of_element)), #If TRUE
+                C + g_k + r * rec_adjusted_length_of_element #If FALSE
+            )
 
-    E_f1 = calculate_exponent(t1half, t2, U, a, b)
-    E_f2 = calculate_exponent(t2, t3, U, a, b)
-    E_f3 = calculate_exponent(t3, t4, U, a, b)
+        E_f1 = calculate_exponent(t1half, t2, U, a, b)
+        E_f2 = calculate_exponent(t2, t3, U, a, b)
+        E_f3 = calculate_exponent(t3, t4, U, a, b)
 
-    E_bar = ( # Sum over the DFE
-        f0 * 0.0
-        + f1 * ((t1half - t1) / (t2 - t1)) * 0.0
-        + f1 * ((t2 - t1half) / (t2 - t1)) * E_f1
-        + f2 * E_f2
-        + f3 * E_f3)
+        E_bar = ( # Sum over the DFE
+            f0 * 0.0
+            + f1 * ((t1half - t1) / (t2 - t1)) * 0.0
+            + f1 * ((t2 - t1half) / (t2 - t1)) * E_f1
+            + f2 * E_f2
+            + f3 * E_f3)    
 
-    return np.exp(-1.0 * E_bar) # Return B
+        B = np.exp(-1.0 * E_bar)
+        
+    return np.where(length_of_element == 0, 1.0, B)
 
 def calculateB_linear_oldGC(distance_to_element, length_of_element):
     """
