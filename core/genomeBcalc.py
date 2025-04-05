901fd8cfb100d36f94c46fb9b337571b6a7359cb
Jacob Marsh <jake.marsh@live.com.au>
2025-04-04 22:13:31 -0400

    Plan genomeBcalc wrapper for chromBcalc to calc for many chromosomes

--- core/genomeBcalc.py
+++ core/genomeBcalc.py
@@ -1,5 +1,15 @@
 from core.chromBcalc import chromBcalc
+import numpy as np
 
 def genomeBcalc(args):    
+
+    unique_chroms = np.unique() # Move BED/GFF handler here
+    # GET LIST OF EACH UNIQUE CHROMOSOME
+    # GET UNLINKED B FOR EACH CHROMOSOME
+    # FOR EACH UNIQUE CHROM
+    # # DO: 
     output_data, block_ranges = chromBcalc(args)
+    # # # NO INPUT COLLECTED!!
+    # # Pass up plot data as needed
+
     return output_data, block_ranges
