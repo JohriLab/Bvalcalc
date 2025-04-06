c9839453830f01787a812222d7e928a03d770dfb
Jacob Marsh <jake.marsh@live.com.au>
2025-04-06 19:23:51 -0400

    Start on looping over chromosomes in genomeBcalc

--- core/genomeBcalc.py
+++ core/genomeBcalc.py
@@ -1,17 +1,28 @@
 from core.chromBcalc import chromBcalc
 from core.utils.bedgffHandler import bedgffHandler
 import numpy as np
+import sys
 
 def genomeBcalc(args):    
 
-    blockstart, blockend, blockchrom = bedgffHandler(args.bedgff_path) # Read BED/GFF, return start and end of conserved elements
+    allblockstart, allblockend, allblockchrom = bedgffHandler(args.bedgff_path) # Read BED/GFF, return start and end of conserved elements
 
-    unique_chroms = np.unique(blockchrom) # Move BED/GFF handler here
-    # GET LIST OF EACH UNIQUE CHROMOSOME
+    unique_chromosomes = np.unique(allblockchrom) # Move BED/GFF handler here
+    print(unique_chromosomes) ## Now, loop over each chromosome and save B output
+    for i in np.arange(0,len(unique_chromosomes)):
+        mask = allblockchrom == unique_chromosomes[i]
+        blockstart = allblockstart[mask]
+        blockend = allblockstart[mask]
+        blockchrom = allblockchrom[mask]
+        print(mask)
+
+    sys.exit()
+    output_data, block_ranges = chromBcalc(args, blockstart, blockend, blockchrom)
+
     # GET UNLINKED B FOR EACH CHROMOSOME
     # FOR EACH UNIQUE CHROM
     # # DO: 
-    output_data, block_ranges = chromBcalc(args, blockstart, blockend, blockchrom)
+    
     # # # NO INPUT COLLECTED!!
     # # Pass up plot data as needed
 
