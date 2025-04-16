3e5b4c4c30dd3bf32c39fcf9965adceec9ae1769
Jacob Marsh <jake.marsh@live.com.au>
2025-04-16 13:36:11 -0400

    Update chromosome name in tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -83,7 +83,7 @@ def test_cli_genome_basic(tmp_path):
     #python Bvalcalc.py --genome --pop_params tests/testparams/nogcBasicParams.py --bedgff_path exampleData/200kb_slimtest.csv --chr_size 200000 --out /path/to/output/dgas.bvals
     script = Path(__file__).resolve().parents[1] / "Bvalcalc.py"
     params = Path(__file__).resolve().parents[1] / "tests" / "testparams" / "nogcBasicParams.py"
-    bed_path = Path(__file__).resolve().parents[1] / "exampleData" / "200kb_slimtest.csv"
+    bed_path = Path(__file__).resolve().parents[1] / "tests" / "testfiles" / "200kb_slimtest.csv"
     chr_sizes_path = Path(__file__).resolve().parents[1] / "exampleData" / "test_sizes.txt"
     output_path = tmp_path / "200kb_dfe5.bvals"
 
@@ -100,7 +100,7 @@ def test_cli_genome_basic(tmp_path):
 
     assert result.returncode == 0, f"CLI failed:\n{result.stderr}"
     out = result.stdout + result.stderr
-    assert "Mean B of neutral sites across chromosome chr_2R: 0.753693843332109" in out
+    assert "Mean B of neutral sites across chromosome chr_200kb: 0.753693843332109" in out
     assert output_path.exists(), "Expected output file not created"
     assert output_path.stat().st_size > 0, "Output file is empty"
 
@@ -125,7 +125,7 @@ def test_cli_genome_gcparams(tmp_path):
     assert result.returncode == 0, f"CLI failed:\n{result.stderr}"
     out = result.stdout + result.stderr
     assert "====== R E S U L T S ====== S U M M A R Y ==========" in out
-    assert "Mean B of neutral sites across chromosome chr_2R: 0.836347850423207" in out
+    assert "Mean B of neutral sites across chromosome chr_200kb: 0.836347850423207" in out
     assert output_path.exists(), "Expected output file not created"
     assert output_path.stat().st_size > 0, "Output file is empty"
 
@@ -152,7 +152,7 @@ def test_cli_genome_with_recmap_plot(tmp_path):
     assert result.returncode == 0, f"CLI failed:\n{result.stderr}"
     out = result.stdout + result.stderr
     assert "Cumulative length of chromosome under selection: 99990bp (50.0%)" in out
-    assert "Mean B of neutral sites across chromosome chr_2R: 0.7015847245703709" in out
+    assert "Mean B of neutral sites across chromosome chr_200kb: 0.7015847245703709" in out
     assert f"Saved B values to: {output_path.as_posix()}" in out
     assert output_path.exists(), "Expected output file not created"
     assert output_path.stat().st_size > 0, "Output file is empty"
@@ -164,9 +164,9 @@ def test_cli_mean_b_value():
         sys.executable, "Bvalcalc.py",
         "--region",
         "--pop_params", "tests/testparams/nogcBasicParams.py",
-        "--bedgff_path", "exampleData/200kb_slimtest.csv",
+        "--bedgff_path", "tests/testfiles/200kb_slimtest.csv",
         "--plot_output",
-        "--calc_region", "chr_2R:1514-62456",
+        "--calc_region", "chr_200kb:1514-62456",
     ], capture_output=True, text=True)
 
     assert result.returncode == 0, f"Process failed: {result.stderr}"
