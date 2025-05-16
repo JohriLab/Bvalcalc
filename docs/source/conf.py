abf8497463b81e9f9d11543b33ce3cf99c07f3cf
Jacob Marsh <jake.marsh@live.com.au>
2025-05-16 12:09:21 -0400

    Right bee trail sticker

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,6 @@ release = '0.2.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-templates_path = ['_templates']
 exclude_patterns = []
 
 
@@ -25,6 +24,7 @@ exclude_patterns = []
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
+templates_path = ['_templates']
 html_show_sphinx = False
 # html_show_copyright = False
 
