915a8f5396911f94afc385432e7223b7662df86e
Jacob Marsh <jake.marsh@live.com.au>
2025-05-16 22:49:31 -0400

    Position absolute bottom for teal and purple

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,6 +22,11 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+    'collapse_navigation': False,
+    'sticky_navigation': True,
+    'navigation_depth': 1,
+}
 html_static_path = ['_static']
 
 templates_path = ['_templates']
