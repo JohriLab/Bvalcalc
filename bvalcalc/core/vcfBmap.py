faad59f33f3994a4860478e9244fcc2098228e88
Jacob Marsh <jake.marsh@live.com.au>
2025-05-21 15:37:13 -0400

    Extend warning messages vcfBmap

--- bvalcalc/core/vcfBmap.py
+++ bvalcalc/core/vcfBmap.py
@@ -5,21 +5,45 @@ import numpy as np
 
 def vcfBmap(args, vcf_path):    
 
-    vcf_chromosomes, vcf_positions = load_vcf(vcf_path)
-    print(vcf_chromosomes, vcf_positions)
+    vcf_chroms, vcf_pos = load_vcf(vcf_path)
+    print(vcf_chroms, vcf_pos)
 
-    bmap_chromosomes, bmap_positions, b_values = load_Bmap(file_path = args.Bmap)
-    print(bmap_chromosomes, bmap_positions, b_values)
+    bmap_chroms, bmap_pos, b_values = load_Bmap(file_path = args.Bmap)
+    print(bmap_chroms, bmap_pos, b_values)
 
-    max_bmap_pos = bmap_positions[-1]
-    above_count  = np.sum(vcf_positions > max_bmap_pos)
+    max_bmap_pos = bmap_pos[-1]
+    above_count  = np.sum(vcf_pos > max_bmap_pos)
     if above_count > 0:
-        print(f"WARNING: {above_count} VCF positions are above the max B-map position ({max_bmap_pos})")
+        print(f"WARNING: {above_count} VCF positions are above the max B-map start position ({max_bmap_pos}) consider calculating an extended B-map")
 
-    idx = np.searchsorted(bmap_positions, vcf_positions, side='right') - 1
+    vcf_unique  = np.unique(vcf_chroms)
+    bmap_unique = np.unique(bmap_chroms)
+    missing_in_bmap = set(vcf_unique) - set(bmap_unique)
+    missing_in_vcf  = set(bmap_unique) - set(vcf_unique)
+    if missing_in_bmap:
+        print(f"WARNING: The following chromosomes are in the VCF but not in the B-map: {missing_in_bmap}")
+    if missing_in_vcf:
+        print(f"WARNING: The following chromosomes are in the B-map but not in the VCF: {missing_in_vcf}")
+
+
+    idx = np.searchsorted(bmap_pos, vcf_pos, side='right') - 1
     vcf_b_values = b_values[idx]
+
+
+
+    # vcf_bvals = np.full(vcf_pos.shape, np.nan, dtype=np.float64)
+
+    #     for chrom in bmap_chroms:
+    #     mask_b = (bmap_chroms_raw == chrom)
+    #     starts = bmap_pos[mask_b]
+    #     vals   = bmap_vals[mask_b]
+
     print(idx, vcf_b_values)
 
     print("gottem in vcfBmap")
 
+# 1. Return histogram of B for VCF positions
+###     Also give B for each VCF position in file
+# 2. Get position of sites with B above or below X, or top N positions of bottom N positions
+###     Also recode VCF with those positions if specified
 
