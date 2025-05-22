0a5bcd9c64ae8f08f4c7e4da084a5b24b6974523
jacobimarsh <jacobimarsh@users.noreply.github.com>
2025-05-22 20:29:52 +0000

    deploy: 4a398a2130a787df01e6445c9a42c0ee57006935

--- tests/test_calculateB.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-from bvalcalc.core.calculateB import calculateB_linear
-
-def test_B_value_for_zero_length():
-    result = calculateB_linear(np.array([100]), np.array([0]))
-    assert np.allclose(result, 1.0), "B should be 1.0 when length_of_element is zero"
