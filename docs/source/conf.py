1af14342f1bee6871b02f03b1d44e0a9db411b26
Jacob Marsh <jake.marsh@live.com.au>
2025-05-22 15:34:00 -0400

    Update license

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = 'Bvalcalc'
 copyright = '2025, Jacob Marsh, Parul Johri'
 author = 'Jacob Marsh, Parul Johri'
-release = '0.3.1'
+release = '0.4.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
