fcd3e5c1ca9f50277367e61308fe6b2a469080c7
Jacob Marsh <jake.marsh@live.com.au>
2025-05-28 18:05:19 -0400

    Padding added to bottom of navbar in short mode

--- tests/testparams/UnlinkedParams.py
+++ tests/testparams/UnlinkedParams.py
@@ -10,7 +10,7 @@
 x = 100 # Scaling factor (N,u,r), keep as 1 unless calculating for rescaled simulations
 Nanc = 1e6/x # Ancestral population size
 r = 0.5*1e-8*x # Recombination (crossover) rate per bp, per generation
-u = 8*3*1e-9*x # Mutation rate (all types) per bp, per generation
+u = 4*3*1e-9*x # Mutation rate (all types) per bp, per generation
 g = 0*1e-8*x # Gene conversion initiation rate per bp, per generation
 k = 440 # Gene conversion tract length (bp)
 
@@ -19,10 +19,10 @@ Ncur = Nanc # Current population size (!Requires --pop_change)
 time_of_change = 1 # Time in 2Ncur generations ago that effective population size went from Nanc to Ncur (!Requires --pop_change)
 
 # Distribution of fitness effects (DFE) parameters (Must equal 1)
-f0 = 0.1 #(*Proportion of effectively neutral mutations with 0 <= |2Nes| < 1 *) 0.25
-f1 = 0.2 #(*Proportion of weakly deleterious mutations with 1 <= |2Nes| < 10 *) 0.6533 0.49
-f2 = 0.3 #(*Proportion of moderately deleterious mutations with 10 <= |2Nes| < 100 *) 0.0533 0.04
-f3 = 0.4 #(*Proportion of strongly deleterious mutations with |2Nes| >= 100 *) 0.2933 0.22
+f0 = 0.5 #(*Proportion of effectively neutral mutations with 0 <= |2Nes| < 1 *) 0.25
+f1 = 0 #(*Proportion of weakly deleterious mutations with 1 <= |2Nes| < 10 *) 0.6533 0.49
+f2 = 0 #(*Proportion of moderately deleterious mutations with 10 <= |2Nes| < 100 *) 0.0533 0.04
+f3 = 0.5 #(*Proportion of strongly deleterious mutations with |2Nes| >= 100 *) 0.2933 0.22
 
 ## Advanced DFE parameters
 gamma_cutoff = 5 # 2Ns threshold for effectively neutral alleles, mutations below this threshold will be ignored in B calculation. Keep as 5 unless theory suggests otherwise.
