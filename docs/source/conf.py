f713e18943d65d2d918a2c1dee2fa8959ccdb6a5
Jacob Marsh <jake.marsh@live.com.au>
2025-07-31 12:41:06 -0400

    Bump to v0.5.4, fixing some bugs and supporting --constant_dfe

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = 'Bvalcalc'
 copyright = '2025, Jacob Marsh, Parul Johri'
 author = 'Jacob Marsh, Parul Johri'
-release = '0.5.3'
+release = '0.5.4'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
