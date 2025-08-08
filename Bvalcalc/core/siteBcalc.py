f3c92cbe2f62bf57c273b1afb736de3cb0c984a6
Jacob Marsh <jake.marsh@live.com.au>
2025-08-08 14:26:11 -0400

    More HRI notes, probably for the paper

--- Bvalcalc/core/siteBcalc.py
+++ Bvalcalc/core/siteBcalc.py
@@ -51,6 +51,28 @@ def siteBcalc(args):
     ##  Option 3b. Calculate B as normal for all non-interfering regions
     ##  Option 3c. For within interference region, use interference B
     ##  Option 3d. For outside interference region, use interference B modified by rec length and rec distance
+    ##
+    ##  Option 4. Calculate b from interfering region as if there's no interference, but algorithmically ensure there's no local dip at the edge of non-recombining regions.
+    ##
+    ##  Consequences: BGS from interference region is overestimated, as it's not accounting for HRI reducing efficacy of selection in interference region
+    ##                  Response: For this reason, our results will not be accurate if a large proportion of the genome experiencing selection also experiences very low recombination.
+    ##                  Response: In other cases, this is only expected to substantially affect nearby flanking regions to a HRI region, particularly due to the effects of weakly and moderately deleterious mutations (f1,f2 class).
+    ##                  Response: To illustrate this effect, consider a HRI region which has a recombination rate of 0 and 10kb of selected sites. 
+    ##                             B' can be well approximated for these HRI region sites by Bvalcalc, even taking into account an accurate estimate of N0 from classic BGS effects (B) across the genome. 
+    ##                             However, within the HRI region with r = 0, the recombinant distance is 0. 
+    ##                             Which means for the first site outside the HRI region, B will be calculated as though there is 10kb of selected sites, but all of them are 1bp away.
+    ##                             This is obviously ineffective, a more rigorous approach would calculate B for these sites considering the lessened effects of selection in the region due to HRI. 
+    ##                             On a deeper level, this is an issue that arises due to the shortcut of partitioning interference regions into discrete categories in spite of the reality that the borders of regions experiencing HRI are diffuse.
+    ##                             Nevertheless, while Bvalcalc does not implement an exact solution to this quandry, we have implemented a practical algorithm to smooth the borders between HRI regions and immediately adjacent sites.
+    ##                             This algorithm runs after B is calculating for all non-HRI sites considering classic BGS from all sites, and after B' is calculated for each HRI region. 
+    ##                                  For each site j distance from the HRI region, where j starts at 1 (bp), compare the B of site j to B' of the flanking HRI region. 
+    #                                   If B' is higher than B, replace B with B' for this site and increment j. 
+    #                                   Continue replacing B with B' until you reach a site distance j away for which B is higher than B', which should happen rapidly once the local effect from f1 and f2 mutations is diminished by recombination
+    #                                   To avoid edge cases where B decreases due to proximity to a new selected region placed a few bp distance from the HRI region, break also if B at site j is higher than at site j+1 (as in, B is decreasing with increasing distance from the HRI region)
+    ##
+    ##  4a. Calculate 
+    ##
+    ##
 
 
     ## linkage_block_max_rec_length = r * 100 # r * l
