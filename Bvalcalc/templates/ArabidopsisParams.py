9e8feb72f1e726678d55909b69c223c4abe003a1
Jacob Marsh <jake.marsh@live.com.au>
2025-08-21 00:11:02 -0400

    Finalize ArabidopsisParams.py

--- Bvalcalc/templates/ArabidopsisParams.py
+++ Bvalcalc/templates/ArabidopsisParams.py
@@ -4,24 +4,27 @@
 ## e.g. Bvalcalc --params path/to/ExampleParams.py
 ##
 ## Core parameters
+f = 0.97 # Selfing rate (f = S/(2-S); Wright's inbreeding coefficient) [1]
 x = 1 # Scaling factor (N,u,r), keep as 1 unless calculating for rescaled simulations
-Nanc = 3.9e5 / x # Ancestral population size [d]
-r = 7.465e-8 * x # Recombination (crossover) rate per bp, per generation (sex-averaged) [a]
-u = 6.95e-9 * x # Mutation rate (all types) per bp, per generation [b]
-g = r / 140 * x # Gene conversion initiation rate per bp, per generation [c]
-k = 50 # Gene conversion tract length (bp) [c]
+Nanc = 1e5 / (1+f) / x # Ancestral population size [2]
+r = 7.465e-8 * (1-f) * x # Recombination (crossover) rate per bp, per generation (sex-averaged) [3]
+u = 6.95e-9 * x # Mutation rate (all types) per bp, per generation [4]
+g = r * 50 * (1-f) * x # Gene conversion initiation rate per bp, per generation [5]
+k = 553 # Gene conversion tract length (bp) [5]
 ## DFE parameters for ALL sites in annotated regions (Sum must equal 1)
-Xf0 = 0.25 # Proportion of effectively neutral mutations with 0 <= |2Ns| < 1 (Note that 2Ns<5 does not contribute to BGS) [4]
-Xf1 = 0.49 # Proportion of weakly deleterious mutations with 1 <= |2Ns| < 10 [4]
-Xf2 = 0.04 # Proportion of moderately deleterious mutations with 10 <= |2Ns| < 100 [4]
-Xf3 = 0.22 # Proportion of strongly deleterious mutations with |2Ns| >= 100 [4]
+f0 = 0.2 # Proportion of effectively neutral mutations with 0 <= |2Ns| < 1 (Note that 2Ns<5 does not contribute to BGS) [6]
+f1 = 0.14 # Proportion of weakly deleterious mutations with 1 <= |2Ns| < 10 [6]
+f2 = 0.44 # Proportion of moderately deleterious mutations with 10 <= |2Ns| < 100 [6]
+f3 = 0.22 # Proportion of strongly deleterious mutations with |2Ns| >= 100 [6]
 ## Demography parameters
-XNcur = 2 * Nanc # Current population size (!Requires --pop_change) [5]
-Xtime_of_change = 0.45 # Time in Nanc generations ago that effective population size went from Nanc to Ncur (!Requires --pop_change) [6]
+Ncur = 0.5 * Nanc # Current population size (!Requires --pop_change) [2]
+time_of_change = 1 # Time in Nanc generations ago that effective population size went from Nanc to Ncur (!Requires --pop_change) [2]
 ## Advanced DFE parameters 
-Xh = 0.5 # Dominance coefficient of selected alleles [Naive value]
-Xmean, shape, proportion_synonymous = 500, 0.5, 0.3 # Gamma distribution of DFE to discretize and replace f0-f3 [mean (2Ns), shape, proportion synonymous] (!Requires --gamma_dfe) [Naive value]## Literature cited
-# [a] Rowan et al. 2019 doi: 10.1534/genetics.119.302406
-# [b] Weng et al. 2018 doi: 10.1534/genetics.118.301721
-# [c] Wijnker et al. 2013 doi: 10.7554/eLife.01426
-# [d] Nordborg et al. 2005 doi: 10.1371/journal.pbio.0030196
+h = 0.5 + (f-0.5*f) # Dominance coefficient of selected alleles [Naive value]
+mean, shape, proportion_synonymous = 500, 0.5, 0.3 # Gamma distribution of DFE to discretize and replace f0-f3 [mean (2Ns), shape, proportion synonymous] (!Requires --gamma_dfe) [Naive value]## Literature cited
+# [1] Platt et al. 2010 doi: 10.1371/journal.pgen.1000843
+# [2] Durvasula et al. 2017 doi: 10.1073/pnas.1616736114
+# [3] Rowan et al. 2019 doi: 10.1534/genetics.119.302406
+# [4] Weng et al. 2018 doi: 10.1534/genetics.118.301721
+# [5] Yang et al. 2012 doi: 10.1073/pnas.1211827110
+# [6] Hämälä and Tiffin 2020 doi: 10.1534/genetics.120.303335
