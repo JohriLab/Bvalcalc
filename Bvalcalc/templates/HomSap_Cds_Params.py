66dfa90e68d616bb91fbfc6b0fad326f6f5cb8d4
Jacob Marsh <jake.marsh@live.com.au>
2025-08-29 17:46:18 -0400

    Update human GC parameters for Palsson map

--- Bvalcalc/templates/HomSap_Cds_Params.py
+++ Bvalcalc/templates/HomSap_Cds_Params.py
@@ -8,8 +8,8 @@ x = 1 # Scaling factor (N,u,r), keep as 1 unless calculating for rescaled simula
 Nanc = 7300 / x # Ancestral population size [1]
 r = 1e-8 * x # Recombination (crossover) rate per bp, per generation (sex-averaged) [2]
 u = 1.25e-8 * x # Mutation rate (all types) per bp, per generation [2]
-g = 2e-8 * x # Gene conversion initiation rate per bp, per generation [3]
-k = 459 # Gene conversion tract length (bp) [3]
+g = 3.27e-8 * x # Gene conversion initiation rate per bp, per generation [3]
+k = 113 # Gene conversion tract length (bp) [3]
 ## DFE parameters for ALL sites in annotated regions (Sum must equal 1)
 f0 = 0.51 # Proportion of effectively neutral mutations with 0 <= |2Ns| < 1 (Note that 2Ns<5 does not contribute to BGS) [4]
 f1 = 0.14 # Proportion of weakly deleterious mutations with 1 <= |2Ns| < 10 [4]
@@ -24,5 +24,5 @@ mean, shape = 500, 0.5 # Gamma distribution of DFE to discretize and replace f0-
 ## Literature cited
 # [1] Gutenkunst et al 2009 doi: 10.1371/journal.pgen.1000695
 # [2] Kong et al 2012 doi: 10.1038/nature11396
-# [3] Masaki and Browning 2025 doi: 10.1101/2024.12.30.630818
+# [3] Palsson 2025 doi: 10.1038/s41586-024-08450-5
 # [4] Huber et al 2017 doi: 10.1073/pnas.1619508114
