0ce015f79e3534ff372462aafb48130e64f8eead
Jacob Marsh <jake.marsh@live.com.au>
2025-09-16 13:38:44 -0400

    Fix chromosome size print statement: show chr_size instead of calc_end

--- Bvalcalc/core/chromBcalc.py
+++ Bvalcalc/core/chromBcalc.py
@@ -48,7 +48,7 @@ def chromBcalc(args, blockstart, blockend, chromosome, unlinked_B, prior_pos = N
     if not args.quiet: 
         print(f"====== P A R A M E T E R S =========================")
         print(f"BED/GFF file for regions under selection: {file_path}")
-        if chr_size is not None: print(f"Last position in chromosome {chromosome}: {calc_end}")
+        if chr_size is not None: print(f"Last position in chromosome {chromosome}: {chr_size}")
         print(f"Size of chunks to calculate B in per iteration: {chunk_size}bp")
         print(f"Number of adjacent chunks to calculate B precisely for: {precise_chunks}")
 
