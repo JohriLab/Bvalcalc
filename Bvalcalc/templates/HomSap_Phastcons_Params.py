06e7e5fdc04638fc2c635c53cbee0c97da9a1e64
Jacob Marsh <jake.marsh@live.com.au>
2025-10-07 11:06:00 -0400

    Rearrange Human Phastcons params

--- Bvalcalc/templates/HomSap_Phastcons_Params.py
+++ Bvalcalc/templates/HomSap_Phastcons_Params.py
@@ -11,19 +11,19 @@ u = 1.25e-8 * x # Mutation rate (all types) per bp, per generation [2]
 g = 3.27e-8 * x # Gene conversion initiation rate per bp, per generation [3]
 k = 113 # Gene conversion tract length (bp) [3]
 ## DFE parameters for ALL sites in annotated regions (Sum must equal 1)
-f0 = 0.78 # Proportion of effectively neutral mutations with 0 <= |2Ns| < 1 (Note that 2Ns<5 does not contribute to BGS) [5]
-f1 = 0.08 # Proportion of weakly deleterious mutations with 1 <= |2Ns| < 10 [5]
-f2 = 0.08 # Proportion of moderately deleterious mutations with 10 <= |2Ns| < 100 [5]
+f0 = 0.58 # Proportion of effectively neutral mutations with 0 <= |2Ns| < 1 (Note that 2Ns<5 does not contribute to BGS) [5]
+f1 = 0.17 # Proportion of weakly deleterious mutations with 1 <= |2Ns| < 10 [5]
+f2 = 0.19 # Proportion of moderately deleterious mutations with 10 <= |2Ns| < 100 [5]
 f3 = 0.06 # Proportion of strongly deleterious mutations with |2Ns| >= 100 [5]
 ## Demography parameters
 Ncur = 14474 # Current population size (!Requires --pop_change) [1]
 time_of_change = 0.81 # Time in Nanc generations ago that effective population size went from Nanc to Ncur (!Requires --pop_change) [1]
 ## Advanced DFE parameters 
 h = 0.5 # Dominance coefficient of selected alleles [Naive value]
-mean, shape, proportion_synonymous = 26.56, 0.0415, 0 # Gamma distribution of DFE to discretize and replace f0-f3 [mean (2Ns), shape] (!Requires --gamma_dfe) [5]
+mean, shape, proportion_synonymous = 26, 0.1165, 0 # Gamma distribution of DFE to discretize and replace f0-f3 [mean (2Ns), shape] (!Requires --gamma_dfe) [5]
 ## Literature cited
 # [1] Gutenkunst et al 2009 doi: 10.1371/journal.pgen.1000695
 # [2] Kong et al 2012 doi: 10.1038/nature11396
 # [3] Palsson 2025 doi: 10.1038/s41586-024-08450-5
 # [4] Huber et al 2017 doi: 10.1073/pnas.1619508114
-# [5] Torgerson et al 2009 doi: 10.1371/journal.pgen.1000592
+# [5] Di et al 2025 doi: 10.1101/2025.05.14.654124
