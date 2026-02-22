0f93b7ae0236e8c3c4fe8e73b4b15504891dd702
Jacob Marsh <jake.marsh@live.com.au>
2026-02-22 16:16:38 +0800

    Update version number to 1.4.1.

--- Bvalcalc/__init__.py
+++ Bvalcalc/__init__.py
@@ -2,7 +2,7 @@
 bvalcalc: calculate relative diversity (B) under background selection.
 """
 
-__version__ = "1.4.0"
+__version__ = "1.4.1"
 
 # Expose main entry point
 from .cli import main
--- Bvalcalc/cli.py
+++ Bvalcalc/cli.py
@@ -12,7 +12,7 @@ from Bvalcalc.core.positionsBstats import positionsBstats
 from Bvalcalc.core.plotChromB import plotChromB
 from Bvalcalc.utils.sample_data import download_sample_data
 
-__version__ = "1.4.0"
+__version__ = "1.4.1"
 
 def main():
     start_time = time.time()
