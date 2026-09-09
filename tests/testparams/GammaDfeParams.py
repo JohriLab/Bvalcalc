4baccec2f3fc50b2c84f9ff43bef546ba7669508
Jacob Marsh <jake.marsh@live.com.au>
2026-09-09 12:18:56 +0800

    Gamma dfe f0-f3 proportions

--- tests/testparams/GammaDfeParams.py
+++ tests/testparams/GammaDfeParams.py
@@ -17,10 +17,10 @@ Ncur = Nanc # Current population size (!Requires --pop_change)
 time_of_change = 1 * Nanc # Time in generations ago that effective population size went from Nanc to Ncur (!Requires --pop_change)
 
 # Distribution of fitness effects (DFE) parameters (Must equal 1)
-f0 = 0.1 #(*Proportion of effectively neutral mutations with 0 <= |2Nes| < 1 *) 0.25
-f1 = 0.2 #(*Proportion of weakly deleterious mutations with 1 <= |2Nes| < 10 *) 0.6533 0.49
-f2 = 0.3 #(*Proportion of moderately deleterious mutations with 10 <= |2Nes| < 100 *) 0.0533 0.04
-f3 = 0.4 #(*Proportion of strongly deleterious mutations with |2Nes| >= 100 *) 0.2933 0.22
+f0 = 0.07602 #(*Proportion of effectively neutral mutations with 0 <= |2Nes| < 1 *) 0.25
+f1 = 0.09283 #(*Proportion of weakly deleterious mutations with 1 <= |2Nes| < 10 *) 0.6533 0.49
+f2 = 0.20287 #(*Proportion of moderately deleterious mutations with 10 <= |2Nes| < 100 *) 0.0533 0.04
+f3 = 0.62829 #(*Proportion of strongly deleterious mutations with |2Nes| >= 100 *) 0.2933 0.22
 
 ## Advanced DFE parameters
 gamma_cutoff = 5 # 2Ns threshold for effectively neutral alleles, mutations below this threshold will be ignored in B calculation. Keep as 5 unless theory suggests otherwise.
